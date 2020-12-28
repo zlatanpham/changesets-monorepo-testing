6efb54b5e0c60954666871abef2acfed97474d53
zlatan <zlatan.pham@gmail.com>
2020-12-28 14:45:16 +0700

    chore: test bump 2 package

---
+++ .changeset/lucky-carrots-compete.md
@@ -0,0 +1,6 @@
+---
+'@tpham0/pa': patch
+'@tpham0/pb': patch
+---
+
+test
--- packages/pa/src/index.ts
+++ packages/pa/src/index.ts
@@ -43,5 +43,5 @@ export function log11(message: string) {
 }
 
 export function log213(message: string) {
-  console.log(message + '222s2 212');
+  console.log(message + '2s2 212');
 }
